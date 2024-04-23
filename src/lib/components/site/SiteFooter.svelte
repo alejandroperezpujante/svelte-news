0448308f3550f79dd4ef84153b2d3f7820d3efa9
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-04-23 10:27:06 +0200

    add footer and theme select

---
+++ src/lib/components/site/SiteFooter.svelte
@@ -0,0 +1,20 @@
+<script>
+	import ThemeSelect from '$lib/components/ThemeSelect.svelte';
+	import { Button } from '../ui/button';
+</script>
+
+<footer class="px-4 py-2 grid grid-cols-3">
+	<ThemeSelect />
+
+	<p class="text-center text-muted-foreground">
+		Made with ❤️ by <Button
+			variant="link"
+			class="p-0"
+			href="https://github.com/alejandroperezpujante"
+			target="_blank"
+			rel="noopener noreferrer">Alejandro Pérez Pujante</Button
+		>
+	</p>
+
+	<div></div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import '../app.pcss';
+
 	import SiteHeader from '$lib/components/site/SiteHeader.svelte';
+	import SiteFooter from '$lib/components/site/SiteFooter.svelte';
 
 	export let data;
 </script>
 
 <SiteHeader isAuthenticated={data.isAuthenticated} />
 <slot />
+<SiteFooter />

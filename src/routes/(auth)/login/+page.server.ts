1bea292c577f8b7e0bd863bac644af9a7cb77cfd
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-04-23 10:27:27 +0200

    add login feature

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,7 +1,12 @@
-import { superValidate } from 'sveltekit-superforms';
+import { fail, setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 import { loginSchema } from './loginSchema';
+import { redirect } from '@sveltejs/kit';
+import { db } from '$lib/server/db/connection';
+import { eq, or } from 'drizzle-orm';
+import { userTable } from '$lib/server/db/schema';
+import { auth, passwordUtils } from '$lib/server/auth';
 
 export async function load() {
 	return {
@@ -10,5 +15,30 @@ export async function load() {
 }
 
 export const actions = {
-	default: async () => {}
+	default: async ({ locals, request, cookies }) => {
+		if (locals.session || locals.user) redirect(302, '/');
+
+		const form = await superValidate(request, zod(loginSchema));
+		if (!form.valid) return fail(400, { form });
+
+		const { identifier, password } = form.data;
+		const user = await db.query.userTable.findFirst({
+			where: or(eq(userTable.email, identifier), eq(userTable.username, identifier))
+		});
+		if (!user)
+			return setError(form, 'identifier', 'User not found with the given email or username');
+		if (!user.passwordHash) return setError(form, 'identifier', 'User has no password');
+
+		const isPasswordValid = await passwordUtils.verify(user.passwordHash, password);
+		if (!isPasswordValid) return setError(form, 'password', 'Invalid password');
+
+		const session = await auth.createSession(user.id, {});
+		const sessionCookie = auth.createSessionCookie(session.id);
+		cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '/',
+			...sessionCookie.attributes
+		});
+
+		redirect(302, '/');
+	}
 };
--- src/routes/(auth)/login/loginSchema.ts
+++ src/routes/(auth)/login/loginSchema.ts
@@ -1,10 +1,7 @@
+import { passwordSchema } from '$lib/validationSchemas';
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-	identifier: z.string().min(1, 'Please enter your email or username'),
-	password: z
-		.string()
-		.min(1, 'Please enter your password')
-		.min(8, 'Password must be at least 8 characters long')
-		.max(48, 'Password must be at most 48 characters long')
+	identifier: z.string().min(5, 'Please enter your email or username'),
+	password: passwordSchema
 });
